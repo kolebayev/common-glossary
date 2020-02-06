4b03037dad618e7287a55a1e787ddab2dbe48ef6
Ilya Kolebayev <kolebayev@gmail.com>
2020-02-06 14:22:40 +0300

    Add header from gpn ui kit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from "./Layout/Layout";
 import TableOfContents from "./TableOfContents/TableOfContents";
 import shownContent from "./Context/shownContent";
 import firebase from "./Firebase";
+import { Header, Logo, SearchBar } from '@gpn-design/uikit';
 
 function App() {
   const [parsedData, setParsedData] = useState('');
@@ -13,27 +14,37 @@ function App() {
     firebase.getData("glossary", "common", setParsedData);
   }, []);
 
+  const headerLeftSide = [
+    {
+      indent: 'l',
+      children: (
+        <Logo>
+          <p className="text text_size_l text_weight_bold">Глоссарий ГПН</p>
+        </Logo>
+      ),
+    },
+    {
+      indent: 'l',
+      children: <SearchBar placeholder={'Я ищу'} label={'Поиск'} />,
+    },
+  ];
+
+  const headerRIghtSide = [
+    {
+      indent: 's',
+      children: (
+        <p className="text text_size_s text_weight_normal text_view_secondary">v 1.0.0</p>
+      )
+    }
+  ];
+
   return (
     <div className="App theme theme_breakpoint_gpn-default theme_color_gpn-default theme_control_gpn-default theme_font_small theme_gap_small theme_size_gpn-default theme_space_gpn-default">
-      <header
-        style={{
-          width: "100vw",
-          height: "65px",
-          backgroundColor: "var(--color-bg-default)",
-          borderBottom: "1px solid var(--color-bg-border)",
-          position: "sticky",
-          top: "0",
-          boxSizing: "border-box"
-        }}
-      ></header>
+      <Header leftSide={headerLeftSide} rightSide={headerRIghtSide}/>
       <Layout
         leftSection={<TableOfContents />}
         rightSection={
           <div>
-            {/* {initialState} */}
-            {/* {
-              console.log(parsedData)
-            } */}
             {parsedData === '' ? null : parsedData.map((items, index) => {
               return (
                   <>
